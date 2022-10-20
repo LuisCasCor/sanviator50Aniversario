c0f3f1fbed617ea8bb36c821d3ef47a3cd26f09a
LuisCasCor <luis.cascor@sanviatorvalladolid.com>
2022-10-20 11:52:40 +0200

    Agregado el proyecto

---
+++ sanviator50aniversario/noticias/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+ 
+class Noticia(models.Model):
+    title= models.CharField(max_length=200,verbose_name="Título")
+    descripcion=models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen",upload_to="noticias")
+    created= models.TimeField(auto_now_add=True,verbose_name="Fecha de Creación")
+    updated=models.TimeField(auto_now=True,verbose_name="Fecha de Actualización")
+ 
+    class Meta:
+        verbose_name = "Noticia"
+        verbose_name_plural= "Noticias"
+        ordering=["-created"]
+ 
+    def __str__(self):
+        return self.title
